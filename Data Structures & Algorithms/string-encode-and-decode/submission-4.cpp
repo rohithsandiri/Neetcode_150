f320340883c98ded72e26e16a563aa629597ed61
SANDIRI ROHITH <rohithsandiri064@gmail.com>
2026-08-09 01:34:25 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.cpp
@@ -0,0 +1,42 @@
+
+class Solution {
+public:
+    string pattern = "##0##0?";
+
+    string encode(vector<string>& strs) {
+        
+        string result = "";
+        
+        int n = strs.size();
+        for(int i = 0; i < n; i++){
+            result += strs[i];
+            result += pattern;
+        }
+        return result;
+    }
+
+    vector<string> decode(string s) {
+        vector<string>result;
+        int i = 0; int j = 0;int n = s.size();
+        int start = 0;
+        string str;
+
+        while(j < n){
+            if (s[j] == pattern[i]) {
+                i++;
+            }
+
+            if (i == pattern.size()) {
+                i = 0;
+                string st = s.substr(start, j - start - pattern.size()+ 1);
+                start = j+1;
+                result.push_back(st);
+            }
+
+            j++;
+        }
+
+        // result.push_back(s.substr(start,start - j));
+        return result;
+    }
+};
